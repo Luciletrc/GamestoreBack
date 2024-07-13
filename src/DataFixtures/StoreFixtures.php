530ff20e092a8fad03911e6874f7e2da607ea2df
Lucile Trichet <luciletrc@gmail.com>
2024-07-13 10:06:45 +0200

    faker

--- src/DataFixtures/StoreFixtures.php
+++ src/DataFixtures/StoreFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Store;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker;
 use Doctrine\Persistence\ObjectManager;
 use Exception;
 use Symfony\Component\Uid\Uuid;
@@ -14,15 +15,17 @@ class StoreFixtures extends Fixture
     /** @throws Exception */
     public function load(ObjectManager $manager): void
     {
+        $faker = Faker\Factory::create();
+
         for ($i_store = 1; $i_store <= 5; $i_store++) {
 
             /** @var Store $store */
-            $title = "Article n°$i_store";
+            $title = "Boutique n°$i_store";
 
             $store = (new Store())
                 ->setUuid(Uuid::v4())
-                ->setStoreName($title)
-                ->setDescription("Description n°$i_store")
+                ->setStoreName($faker->company())
+                ->setDescription($faker->text())
                 ->setOpeningTime(new DateTimeImmutable())
                 ->setClosingTime(new DateTimeImmutable())
                 ->setCreatedAt(new DateTimeImmutable());
